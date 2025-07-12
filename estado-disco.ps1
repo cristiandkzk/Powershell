616b52a83858f1c26ad6b11843e0bbb68c1cc0bd
Cristian Diaz <cristiandkzk@gmail.com>
2025-07-11 23:33:23 -0300

    Delete estado-disco.ps1

--- estado-disco.ps1
+++
@@ -1,2 +0,0 @@
-# Estado de discos por volumen
-Get-PSDrive -PSProvider 'FileSystem' | Select-Object Name, @{Name="Tamaño (GB)";Expression={[math]::Round($_.Used/1GB + $_.Free/1GB,2)}}, @{Name="Libre (GB)";Expression={[math]::Round($_.Free/1GB,2)}} | Format-Table
